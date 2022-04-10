ef0fa64949eeaacae345cc7d8f18eecf912dbe70
Apoorv Mangalam <53372470+A-K-M16@users.noreply.github.com>
2022-04-10 22:39:18 +0530

    Update Creating_Tables.sql

--- Creating_Tables.sql
+++ Creating_Tables.sql
@@ -1,5 +1,5 @@
 Create Table Employees (
-	Emp_id		Number(10) PRIMARY KEY,
+	Emp_id			Number(10) PRIMARY KEY,
 	Name			varchar2(10),
 	Department		varchar2(10),
 	Designation		varchar2(10),
@@ -7,8 +7,8 @@ Create Table Employees (
 );
 
 Create Table Employee_Details (
-	DOB			Date,
-	Phone_no		Number(10),
+	DOB		Date,
+	Phone_no	Number(10),
 	E_mail		varchar2(10),
 	Address		varchar2(10),
 	Emp_id		Number(10) Foreign Key references   
@@ -18,6 +18,6 @@ Employees(Emp_id)
 Create Table Accounts(
 	Salary		Number(10),
 	Bonus		Number(10),
-	Expenditure		Number(10),
+	Expenditure	Number(10),
 	Emp_id		Number(10) Foreign Key references Employees(Emp_id)
 );
